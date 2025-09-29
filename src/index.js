dae0edb42c0bdf2c0f935fbbdae38eca311d5199
jaykkumar01 <jaytechbc@gmail.com>
2025-09-29 21:48:42 +0530

    send metadata, receive meta data

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { LogProvider } from "./contexts/LogContext";
 import { PeerProvider } from "./contexts/PeerContext";
-import { FileProvider } from "./contexts/FileContext"; // added FileProvider
+import { FileProvider } from "./contexts/FileContext";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <HashRouter>
-        <LogProvider>
-            <PeerProvider>
-                <FileProvider>
-                    <App />
-                </FileProvider>
-            </PeerProvider>
-        </LogProvider>
-    </HashRouter>
+    <React.StrictMode>
+        <HashRouter>
+            <LogProvider>
+                <PeerProvider>
+                    <FileProvider>
+                        <App />
+                    </FileProvider>
+                </PeerProvider>
+            </LogProvider>
+        </HashRouter>
+    </React.StrictMode>
 );
 
 // For performance measuring, you can pass a function to log results
